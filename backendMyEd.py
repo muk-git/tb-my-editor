1cae1f15c3f7b084d0310bfdee5dff2c78d227b2
Mukesh Rathor <mrathor@nvidia.com>
2021-11-08 10:50:31 -0800

    Get it working reliably  - break messages into smaller parts for very long messages  - fix issues with python 3 like using buffered stdin in all places etc
    Works reliably now!

--- backendMyEd.py
+++ backendMyEd.py
@@ -5,6 +5,7 @@ import json
 import sys
 import struct
 import subprocess
+import time
 
 # NOTES:
 #  - print here will send back to the front end, and won't work.
@@ -14,50 +15,105 @@ DEBUG=1
 global DBGFD
 global FILE_CONTAINING_CMD_TORUN
 FILE_CONTAINING_CMD_TORUN="tb-my-ed.cmd"
+global mysrc
 
 def dbgwr(data):
     global DEBUG, DBGFD
 
     if DEBUG:
         DBGFD.write(data)
+        DBGFD.write("\n")
         DBGFD.flush()
     return
+
+# Encode a message for transmission, given its content.
+def encode_message_v2(message_content):
+    encoded_content = json.dumps(message_content)
+    encoded_length = struct.pack('=I', len(encoded_content))
+    return {'length': encoded_length, 'content': encoded_content}
+
+# Send an encoded message to stdout.
+def send_reply_v2(msg_str):
+    encoded_msg = encode_message_v2(msg_str)
+    sys.stdout.write(encoded_msg['length'])
+    sys.stdout.write(encoded_msg['content'])
+    sys.stdout.flush()
+
+def encode_message_v3(message_content):
+    encoded_content = json.dumps(message_content).encode("utf-8")
+    encoded_length = struct.pack('=I', len(encoded_content))
+    #  use struct.pack("10s", bytes), to pack a string of the length of 
+    #  10 characters
+    return {
+        'length': encoded_length, 
+        'content': struct.pack(str(len(encoded_content))+"s",encoded_content)
+           }
+
+def send_reply_v3(msg_str):
+    encoded_msg = encode_message_v3(msg_str)
+    sys.stdout.buffer.write(encoded_msg['length'])
+    sys.stdout.buffer.write(encoded_msg['content'])
+    sys.stdout.buffer.flush()
+
+def send_reply(msg_str):
+    if sys.version_info < (3, 0):
+        send_reply_v2(msg_str)
+    else:
+        send_reply_v3(msg_str)
 
 # Read a message from stdin and decode it. Message contains:
 #   numbytes in binary + payload in ascii with quotes 
 # Eg, payload xyz, then: 5000 followed by "xyz", ie, 050000002278797A22
+#
+# Caveats:
+#   o a newline is considered one byte by JS, but when we receives it's two "\n"
+#     hence can't use msg size for communication protocol
+#
 def get_message():
-    pidstr = str(os.getpid());
+    global mysrc
+    fullmsg = '""'     # create empty string with double quotes
 
-    # payload size is 4 bytes binary, read that. In python3, buffered read for
-    # binary data, default is string.
-    if sys.version_info < (3, 0):
-        len_field = sys.stdin.read(4)
-    else:
-        len_field = sys.stdin.buffer.read(4)
-
-    # dbgwr("len_field val: " + len_field + "\n");
+    # python does not have do while loop.
+    while True:
+        # payload size is 4 bytes binary, read that. 
+        len_field = mysrc.read(4)
+        # dbgwr("len_field val: " + str(len_field))
      
-    if not len_field:
-        sys.exit(0)
+        if not len_field:
+            dbgwr("invalid len_field val: " + str(len_field))
+            sys.exit(0)
 
-    # first byte out of 4 is the length, extract it
-    message_length = struct.unpack('=I', len_field)[0]
+        # first byte out of 4 is the length, extract it for debug 
+        msg_len = struct.unpack('=I', len_field)[0]
+        dbgwr("pid:" + str(os.getpid()) + " Will read " + str(msg_len) +
+              " bytes from stdin")
 
-    # now read that many characters.
-    dbgwr("tbvim2: pid:" + pidstr + " Will read " + str(message_length) +
-          " bytes from stdin\n")
-    message = sys.stdin.read(message_length)
-    dbgwr("tbvim4: message was read from stdin\n")
-    # dbgwr("tbvim4:" + message + "\n")
+        # don't use msg_len for anything: sender single newline for example
+        # gets converted to two chars "\n". Thus if sender sends 5 :abcd\n:
+        # we get 8 including two double quotes: " + abcd + \ + n "
+        msg = mysrc.read(msg_len)
+        if sys.version_info >= (3, 0):
+            msg = msg.decode("utf-8")   # string from binary data
 
-    return json.loads(message)
+        # dbgwr("got msg: " + msg + " len: " + str(len(msg)))
+        # dbgwr("read: " + str(msg_len) + " bytes. msg.size:" + str(len(msg)))
+
+        if msg[1:-1] == "tbMyEd-done-done-done":
+            break
+        else:
+            # remove trailing double quote and leading double quote
+            fullmsg = fullmsg[:-1] + msg[1:]
+            send_reply("tbMyEd-ack")
+            # time.sleep(1)
+
+    # json.loads(str): take a json string and convert to python dictionary
+    return json.loads(fullmsg or 'null')
 
 
 def read_cmd_from_file():
     fd = open(FILE_CONTAINING_CMD_TORUN, 'r')
     cmdline = fd.read()
-    dbgwr("cmdline from file: :" + cmdline + ":\n")
+    # dbgwr("cmdline from file: :" + cmdline + ":")
     fd.close
 
     cmdline = cmdline.strip()        # remove leading/trailing spaces
@@ -83,57 +139,42 @@ def invoke_vim(msg):
           # " -dc -bc -rw +sb -sl 5000 -ut +cn -geometry 80x64 -e /bin/vim "
     cmd = read_cmd_from_file()
     cmd += " " + tmpfn
-    dbgwr("cmd is: " + cmd + "\n")
+    dbgwr("cmd is: " + cmd)
 
     # in python 3, change this to subprocess.run()
     p = subprocess.call(cmd, shell=True, stdout=subprocess.PIPE)
 
-    dbgwr("invoke_vim done:\n")
+    dbgwr("invoke_vim done....")
 
     return tmpfn
     
 
-# Encode a message for transmission, given its content.
-def encode_message(message_content):
-    encoded_content = json.dumps(message_content)
-    encoded_length = struct.pack('=I', len(encoded_content))
-    return {'length': encoded_length, 'content': encoded_content}
-
-# Send an encoded message to stdout.
-def send_message_v2(encoded_message):
-    sys.stdout.write(encoded_message['length'])
-    sys.stdout.write(encoded_message['content'])
-    sys.stdout.flush()
-
-def send_message_v3(encoded_message):
-    sys.stdout.buffer.write(encoded_message['length'])
-    sys.stdout.write(encoded_message['content'])
-    sys.stdout.flush()
-
 def main(args):
-    global DEBUG, DBGFD
+    global DEBUG, DBGFD, mysrc
 
     if DEBUG:
         DBGFD = open('/tmp/tb-myed.out', 'a+')
-        dbgwr("/tmp/tb-myed.out opened/created\n\n")
-        sys.stderr = DBGFD;
+        dbgwr("===========================================================")
+        dbgwr("/tmp/tb-myed.out opened/created\n")
+        sys.stderr = DBGFD
     else:
         DBGFD = -1
 
-    message = get_message()
-    dbgwr("msg type is: " + str(type(message)) + "\n")
+    if sys.version_info < (3, 0):
+        mysrc = sys.stdin
+    else:
+        mysrc = sys.stdin.buffer
 
-    # message type is unicode, so convert to ascii, otherwise it will the 
+    message = get_message()    # returns type string
+
+    # message type is unicode, so convert to ascii, otherwise it will fail the 
     # write to file if there any unicode non-ascii characters.
     message = message.encode('ascii', 'replace')
     tmpfn = invoke_vim(message)
     
     with open(tmpfn) as file:
         data = file.read()
-        if sys.version_info < (3, 0):
-            send_message_v2(encode_message(data))
-        else:
-            send_message_v3(encode_message(data))
+        send_reply(data)
 
     dbgwr("\n")
     if DEBUG:
